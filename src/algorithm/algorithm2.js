5deeb0c907ef2af76464f1db65fcc4eb794c8164
Tomasz Fiechowski <tomasz.fiechowski@gmail.com>
2016-12-13 02:28:34 +0000

    Algorithm for ELEVATOR_STOPPED emulation

--- src/algorithm/algorithm2.js
+++ src/algorithm/algorithm2.js
@@ -12,33 +12,52 @@ function resetFloor(floor) {
 
 }
 
+var floorQueue = [];
+
 interaction.Observable.on(interaction.EVENTS.CALL, (callRequest) => {
     debug("Recieved CALL event");
     var floor = callRequest.floor;
 
     if (callRequest.up == true) {
         elevatorConsole.up.push(floor);
+        floorQueue.push(floor);
     }
 
     if (callRequest.down == true) {
         elevatorConsole.down.push(floor);
+        floorQueue.push(floor);
     }
 });
 
 interaction.Observable.on(interaction.EVENTS.REQUEST_MOVE, (moveRequest) => {
     debug("Recieved move request!");
     Array.merge(elevatorConsole.destinationFloors, moveRequest.destinationFloors);
+
+    Array.merge(floorQueue, moveRequest.destinationFloors);
 });
 
 state.Observable.on(state.EVENTS.ELEVATOR_STOPPED, (floor) => {
     debug("Elevator stopped at floor " + floor + ". Choosing next floor to go to.");
 
-    if (state.getCurrentDirection() == stateConstants.DIRECTION.UP) {
-        elevatorConsole.up.remove(floor);
-    }
-    if (state.getCurrentDirection() == stateConstants.DIRECTION.DOWN) {
-        elevatorConsole.down.remove(floor);
-    }
+    // Those ifs are commented out for purpose of ELEVATOR_STOPPED emulation
+
+    //    if (state.getCurrentDirection() == stateConstants.DIRECTION.UP) {
+    elevatorConsole.up.remove(floor);
+    //  }
+    //if (state.getCurrentDirection() == stateConstants.DIRECTION.DOWN) {
+    elevatorConsole.down.remove(floor);
+    //}
 
     elevatorConsole.destinationFloors.remove(floor);
 });
+
+var intervalCounter = 0;
+setInterval(() => {
+    if (floorQueue.length > 0) {
+        debug("Interval #" + intervalCounter + "\tQueue: " + floorQueue);
+        var nextFloor = floorQueue.shift();
+
+        state.Observable.emit(state.EVENTS.ELEVATOR_STOPPED, nextFloor);
+        intervalCounter++;
+    }
+}, 2000);
